441daeb91cf375633b7a62a759e4d213a9dbc614
Farid_M <farid.m.63@gmail.com>
2016-02-08 21:44:46 +0100

    Take into account parenthesis to place operator correctly

--- 3-ONP/onp.c
+++ 3-ONP/onp.c
@@ -1,91 +1,84 @@
 #include <stdio.h>
-#define EXPR_MAX_SIZE 400
+#define MAX_NUM_OF_LINES 100
+#define EXPR_MAX_SIZE 401
 #define NUM_OF_OPERATOR 5
 
-void convertToRpn(char *, char *);
+void convertToRpn(char *);
 int isParenthesis(char);
 int isOperator(char);
-void pr(char, char *, int);
+void pr(char *);
 
 int main(void) {
 
 	int numOfLine;
+	char lines[MAX_NUM_OF_LINES][EXPR_MAX_SIZE];
 
 	scanf("%d\n", &numOfLine);
 	
 	for (int i = 0; i < numOfLine; i++) {
-		char expr[EXPR_MAX_SIZE] = {'\0'};
-		char rpn[10];
-			
-		fgets(expr, EXPR_MAX_SIZE, stdin);
-		convertToRpn(expr, rpn);
+		fgets(lines[i], EXPR_MAX_SIZE, stdin);
+	}
+
+	for (int d = 0; d < numOfLine; d++) {
+		convertToRpn(lines[d]);
 	}
 
 	return 0;
 }
 
-void convertToRpn(char *expr, char *rpn) {
-	char charStack[EXPR_MAX_SIZE] = {'\0'};;
+void convertToRpn(char *expr) {
+	char charStack[EXPR_MAX_SIZE] = {'\0'};
 	int stackIndex = 0;
 	int count = 0;
 	char temp[EXPR_MAX_SIZE] = {'\0'};
 	int tempIndex = 0;
-	//int parenthesisNum = 0;
-	//int countTwo = 0;
+	int parenthesisNum = 0;
+	int total = 0;
 
 	for (int i = EXPR_MAX_SIZE - 1; i >= 0; i--) {
-		/*		
 		if (expr[i] == ')') {
 			parenthesisNum++;
+			count = 0;
 		} else if (expr[i] == '(') {
 			parenthesisNum--;
-			countTwo = 0;
 		} else 
-		*/
-
-		if (expr[i] == ')') {
-			count = 0;
-		}
 
 		if (expr[i] == '\0' || expr[i] == '\n' || isParenthesis(expr[i])) {
 			//do nothing
 		} else if (isOperator(expr[i])) {
-			//int ref = parenthesisNum ? countTwo : count;	
-			int ref = count;
+			int ref = (parenthesisNum == 1) ? total : count;
 	
 			for (int j = 0; j < ref; j++) {
 				temp[tempIndex++] = charStack[--stackIndex];
 				count--;
+				total--;
 			}
 
 			charStack[stackIndex++] = expr[i];
 			count++;
+			total++;
 			
 			while (tempIndex > 0) {
 				charStack[stackIndex++] = temp[--tempIndex];
 				count++;
+				total++;
 			}
 		} else {
 			charStack[stackIndex++] = expr[i];
 			count++;
+			total++;
 		}
- 
-		printf("\n");
 	}
 	
-	pr ('s', charStack, stackIndex);
-	
-	rpn[0] = 'a';
+	pr (charStack);
 }
 
-//TODO delete that it's just for dev
-void pr(char c, char *p, int ind) {
+void pr(char *p) {
 
 	for (int z = EXPR_MAX_SIZE - 1; z >= 0; z--) {
 		printf("%c", p[z]);
 	}
-	c = c;
-	ind = ind;	
+	printf("\n");
 }
 
 int isParenthesis(char c) {
